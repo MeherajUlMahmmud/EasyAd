92fef2dd40f63bb47e473a11bbb85d548cd1cb47
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-04-03 15:43:52 +0600

    Update models.py

--- ad_control/models.py
+++ ad_control/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from user_control.models import User, CustomerModel
 
-# Create your models here.
+
+class AdvertiseModel(models.Model):
+    ADVERTISEMENT_TYPE_CHOICES = [
+        ('Bill Board', 'Bill Board'),
+        ('Vehicle', 'Vehicle'),
+    ]
+
+    PRICE_RATE_TYPE_CHOICES = [
+        ('Day', 'Day'),
+        ('Month', 'Month'),
+    ]
+
+    SIZE_SCALE_TYPE_CHOICES = [
+        ('Inches', 'Inches'),
+        ('Feet', 'Feet'),
+        ('Meter', 'Meter'),
+    ]
+
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    location = models.CharField(max_length=255)
+    facing = models.CharField(max_length=255, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_rate = models.CharField(max_length=30, choices=PRICE_RATE_TYPE_CHOICES)
+    height = models.DecimalField(max_digits=5, decimal_places=2)
+    width = models.DecimalField(max_digits=5, decimal_places=2)
+    size_scale = models.CharField(max_length=30, choices=SIZE_SCALE_TYPE_CHOICES)
+    advertisement_type = models.CharField(max_length=30, choices=ADVERTISEMENT_TYPE_CHOICES)
+    additional_note = models.TextField(null=True, blank=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.location
+
+
+class OrderModel(models.Model):
+    PRICE_RATE_TYPE_CHOICES = [
+        ('Day', 'Day'),
+        ('Month', 'Month'),
+    ]
+
+    customer = models.ForeignKey(CustomerModel, null=True, blank=True, on_delete=models.CASCADE)
+    advertise = models.ForeignKey(AdvertiseModel, null=True, blank=True, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    duration = models.IntegerField(max_length=10)
+    price_rate = models.CharField(max_length=30, choices=PRICE_RATE_TYPE_CHOICES)
+    total_cost = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    is_paid = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=False)
+    is_running = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+class OrderPaymentModel(models.Model):
+    order = models.ForeignKey(OrderModel, null=True, blank=True, on_delete=models.CASCADE)
+    transaction_id = models.CharField(max_length=50, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    is_paid = models.BooleanField(default=False)
+    date_time = models.DateTimeField(auto_now_add=True)
