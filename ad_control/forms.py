361ae508635dc59af75b7d5c357ea052b6f850a4
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-04-04 14:56:49 +0600

    Update forms.py

--- ad_control/forms.py
+++ ad_control/forms.py
@@ -14,12 +14,18 @@ class PostAdForm(ModelForm):
 
 
 class ConfirmOrderForm(ModelForm):
+    PRICE_RATE_TYPE_CHOICES = [
+        ('Day', 'Day'),
+        ('Month', 'Month'),
+    ]
+
     image = forms.ImageField(required=True, error_messages={'invalid': "Image files only"}, widget=forms.FileInput)
-    is_active = forms.BooleanField(initial=False)
-    is_approved = forms.BooleanField(initial=False)
-    is_running = forms.BooleanField(initial=False)
+    duration = forms.CharField(widget=forms.TextInput(attrs={'id': 'duration'}))
+    price_rate = forms.CharField(widget=forms.Select(
+        choices=PRICE_RATE_TYPE_CHOICES,
+        attrs={'id': 'price_rate'}))
 
     class Meta:
         model = OrderModel
         fields = '__all__'
-        exclude = ['customer', 'advertise', 'total_cost']
+        exclude = ['customer', 'advertise', 'total_cost', 'is_approved', 'is_paid', 'is_running', 'is_complete']
