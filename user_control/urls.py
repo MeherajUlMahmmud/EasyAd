c4910b9f2e515198d998394861e4927dadab30af
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-04-06 00:02:52 +0600

    Update urls.py

--- user_control/urls.py
+++ user_control/urls.py
@@ -6,19 +6,17 @@ urlpatterns = [
     path('', home_view, name='home'),
 
     # user login, logout and registration url
-    path('account/login', login_view, name='login'),
-    path('account/logout', logout_view, name='logout'),
-    path('account/advertiser-registration', advertiser_signup_view, name='advertiser-register'),
-    path('account/customer-registration', customer_signup_view, name='customer-register'),
+    path('accounts/login/', login_view, name='login'),
+    path('accounts/logout/', logout_view, name='logout'),
+    path('accounts/advertiser-registration', advertiser_signup_view, name='advertiser-register'),
+    path('accounts/customer-registration', customer_signup_view, name='customer-register'),
 
     # applicant and company feed url
     path('advertiser/advertiser-dashboard', advertiser_dashboard, name='advertiser-dashboard'),
     path('customer/customer-dashboard', customer_dashboard, name='customer-dashboard'),
-    # path('advertiser/search-result', search_result, name='search-result'),
 
-    # applicant profile, public profile and company profile url
-    # path('advertiser/advertiser-profile/<str:pk>/', advertiser_profile, name='advertiser-profile'),
-    # path('company/company-profile/<str:pk>/', company_profile, name='company-profile'),
+    # advertiser profile and customer profile
+    path('accounts/profile/<str:slug>/', profile_view, name='profile'),
 
     # advertiser and company profile edit url
     # path('advertiser/advertiser-edit-profile', advertiser_edit_profile, name='advertiser-edit-profile'),
