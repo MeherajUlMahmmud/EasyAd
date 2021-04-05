b2e4fee84990393135bd4cf217f8529212839c5c
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-04-06 00:02:41 +0600

    Update urls.py

--- ad_control/urls.py
+++ ad_control/urls.py
@@ -5,5 +5,17 @@ urlpatterns = [
     path('post-ad/', post_ad_view, name='post-ad'),
     path('<str:pk>', ad_detail_view, name='ad-detail'),
     path('confirm-order/<str:pk>', confirm_order_view, name='confirm-order'),
-    path('checkout/', checkout_view, name='checkout'),
+    path('order-details/<str:pk>', order_details_view, name='order-details'),
+
+    path('advertiser/unchecked-orders/', advertiser_unchecked_order_view, name='advertiser-unchecked-orders'),
+    path('advertiser/unpaid-orders/', advertiser_unpaid_order_view, name='advertiser-unpaid-orders'),
+    path('advertiser/ads-to-run/', advertiser_ads_to_run_view, name='advertiser-ads-to-run'),
+    path('advertiser/running-ads/', advertiser_running_ads_view, name='advertiser-running-ads'),
+    path('advertiser/finished-ads/', advertiser_finished_ads_view, name='advertiser-finished-ads'),
+
+    path('customer/pending-orders/', customer_pending_order_view, name='customer-pending-orders'),
+    path('customer/unpaid-orders/', customer_unpaid_order_view, name='customer-unpaid-orders'),
+    path('customer/ads-to-run/', customer_ads_to_run_view, name='customer-ads-to-run'),
+    path('customer/running-ads/', customer_running_ads_view, name='customer-running-ads'),
+    path('customer/finished-ads/', customer_finished_ads_view, name='customer-finished-ads'),
 ]
