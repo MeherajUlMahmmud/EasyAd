8373a16b3aeb1df7df46ed514cc229bdabd9a418
Meheraj <57069206+superior-prog@users.noreply.github.com>
2021-04-06 00:03:37 +0600

    Update admin.py

--- ad_control/admin.py
+++ ad_control/admin.py
@@ -13,4 +13,30 @@ class AdvertiseAdmin(admin.ModelAdmin):
     list_filter = ('user', 'location', 'height', 'width')
 
 
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'is_approved', 'is_canceled', 'advertiser_paid_approval', 'customer_paid_approval',
+                    'is_running', 'is_complete')
+    search_fields = ('customer',)
+    readonly_fields = ()
+
+    filter_horizontal = ()
+    ordering = ('-date_created',)
+    fieldsets = ()
+    list_filter = ('customer', 'is_approved', 'is_canceled', 'advertiser_paid_approval', 'customer_paid_approval',
+                   'is_running', 'is_complete')
+
+
+# class OrderPaymentModelAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'location', 'height', 'width', 'price')
+#     search_fields = ('user', 'location',)
+#     readonly_fields = ()
+#
+#     filter_horizontal = ()
+#     ordering = ('-date_posted',)
+#     fieldsets = ()
+#     list_filter = ('user', 'location', 'height', 'width')
+
+
 admin.site.register(AdvertiseModel, AdvertiseAdmin)
+admin.site.register(OrderModel, OrderModelAdmin)
+admin.site.register(OrderPaymentModel)
